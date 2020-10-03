060c8e5178d8a2d42ccf5b4e4efa21b55ddf1bab
Arthur Fedotiev <artur.fedotiew@gmail.com>
2020-10-03 09:53:44 +0300

    Ready snippet_1

---
+++ src/pages/FilmsPage/components/App.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+import FilmsList from "pages/FilmsPage/components/FilmsList";
+import {films} from "data";
+
+class App extends Component {
+  state = {
+    films: [],
+  };
+  componentDidMount() {
+    this.setState({films});
+  }
+
+  render() {
+    const {films} = this.state;
+    return (
+      <div className="ui container mt-3">
+        <FilmsList films={films} />
+      </div>
+    );
+  }
+}
+
+export default App;
