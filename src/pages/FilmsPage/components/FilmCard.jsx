077816209ce552c7d87ca5585e14e8e383148d83
Arthur Fedotiev <artur.fedotiew@gmail.com>
2020-10-06 08:23:52 +0300

    Ready lesson 1

--- src/pages/FilmsPage/components/FilmCard.jsx
+++ src/pages/FilmsPage/components/FilmCard.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, {memo} from "react";
+import PropTypes from "prop-types";
+import Featured from "components/Featured";
 
 const FilmCard = ({film}) => {
   return (
     <div className="ui card">
-      <span className="ui right corner label">
-        <i className="empty star icon"></i>
-      </span>
+      <Featured featured={film.featured} id={film._id} />
       <div className="image">
         <span className="ui green label ribbon">$ {film.price} </span>
         <img src={film.img} alt="" />
@@ -34,8 +34,20 @@ const FilmCard = ({film}) => {
   );
 };
 
+FilmCard.propTypes = {
+  film: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    director: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    duration: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
+    featured: PropTypes.bool.isRequired,
+  }).isRequired,
+};
+
 FilmCard.defaultProps = {
   film: {},
 };
 
-export default FilmCard;
+export default memo(FilmCard);
